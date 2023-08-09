160e1c0aa745fb9fdbd3c83fd3ddd84caf979882
shrutisharma41 <140740010+shrutisharma41@users.noreply.github.com>
2023-08-09 23:09:15 +0530

    register_page.dart

--- pages/register_page.dart
+++ pages/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -17,11 +18,45 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage>{
   final emailTextController=TextEditingController();
   final passwordTextController=TextEditingController();
-  void signIn()async{
- await FirebaseAuth.instance.signInWithEmailAndPassword(
-  email: emailTextController.text, 
-  password: passwordTextController.text,
-  );
+  final confirmPasswordTextController=TextEditingController();
+
+  void signUp() async {
+ showDialog(context: context,
+  builder: (context)=>const Center(
+    child:CircularProgressIndicator() ,
+    ),
+    );
+    if(passwordTextController.text !=confirmPasswordTextController.text){
+      Navigator.pop(context);
+      displayMessage("Passwords don't match!");
+      return;
+    }
+    try{
+     UserCredential userCredential= await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailTextController.text,
+         password: passwordTextController.text,
+         );
+         FirebaseFirestore.instance.collection(
+          "Users"
+          ).doc(userCredential.user!.email)
+          .set({
+            'username':emailTextController.text.split('@')[0],
+            'bio':'Empty bio...'
+          });
+          
+         if(context.mounted)Navigator.pop(context);
+    } on FirebaseAuthException catch(e){
+      Navigator.pop(context);
+      displayMessage(e.code);
+    }
+  }
+  void displayMessage(String message){
+    showDialog(
+      context: context, 
+      builder: (context)=> AlertDialog(
+        title: Text(message),
+      ),
+      );
   }
   @override
   Widget build(BuildContext context) {
@@ -31,59 +66,68 @@ class _RegisterPageState extends State<RegisterPage>{
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal:25.0),
 
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.lock,
-              size: 100,
-              
-              ),
-             const SizedBox(height:50),
-              Text(
-                "Welcome Back,you have been missed!",
-                style: TextStyle(
-                      color: Colors.grey[700],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.lock,
+                size: 100,
+                
+                ),
+               const SizedBox(height:50),
+                Text(
+                  "Let's create an account for you",
+                  style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                ),
+                const SizedBox(height: 25),
+                MyTextField(
+                  controller: emailTextController,
+                   hintText: 'Email', 
+                   obscureText:false,
+                   ),
+                  const SizedBox(height: 10),
+                   MyTextField(
+                  controller: passwordTextController,
+                   hintText: 'Password', 
+                   obscureText:true,
+                   ),
+                   const SizedBox(height: 10),
+                   MyTextField(
+                  controller: confirmPasswordTextController,
+                   hintText: 'Confirm Password', 
+                   obscureText:true,
+                   ),
+                   const SizedBox(height: 25), 
+                   MyButton(
+                    onTap: signUp,
+                    text: 'Sign Up'
                     ),
-              ),
-              const SizedBox(height: 25),
-              MyTextField(
-                controller: emailTextController,
-                 hintText: 'Email', 
-                 obscureText:false,
-                 ),
-                const SizedBox(height: 10),
-                 MyTextField(
-                controller: passwordTextController,
-                 hintText: 'Password', 
-                 obscureText:true,
-                 ),
-                 const SizedBox(height: 10), 
-                 MyButton(onTap: signIn,
-                  text: 'Sign In'
-                  ),
-                  const SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                    Text("Not a member?",
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                    ),),
-                    const SizedBox(width: 4),
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child:const Text("Register now",
-                      style:TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ) ,),
+                    const SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Text("Already have a account",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),),
+                      const SizedBox(width: 4),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child:const Text("Login now",
+                        style:TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ) ,),
+                      ),
+                    ],
                     ),
-                  ],
-                  ),
-
-            ],
-            ),
+          
+              ],
+              ),
+          ),
         ),
       ),
     ),
