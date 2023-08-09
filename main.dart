fb0f070a0b615c2d5de944a2400015e02da7cab5
shrutisharma41 <140740010+shrutisharma41@users.noreply.github.com>
2023-08-09 22:57:55 +0530

    main.dart

--- main.dart
+++ main.dart
@@ -1,34 +1,31 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:socialmedia_app/auth/auth.dart';
-
+import 'package:socialmedia_app/firebase_options.dart';
+import 'package:socialmedia_app/theme/dark_theme.dart';
+import 'package:socialmedia_app/theme/light_theme.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  
-  
-
-
-
-  // ignore: prefer_typing_uninitialized_variables
-  var defaultFirebaseOptions;
   await Firebase.initializeApp(
-    options: defaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(  MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+     MyApp({Key?key}):super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const AuthPage(),
     
     );
   }
   }
+
 
