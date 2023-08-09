c680a2405cd0e8df5f17c03d568d654813d074f1
shrutisharma41 <140740010+shrutisharma41@users.noreply.github.com>
2023-08-09 23:07:35 +0530

    login_page.dart

--- pages/login_page.dart
+++ pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:socialmedia_app/components/button.dart';
 import 'package:socialmedia_app/components/text_field.dart';
@@ -12,9 +13,34 @@ State<LoginPage> createState()=>_LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final emailTextController=TextEditingController();
+ final emailTextController=TextEditingController();
   final passwordTextController=TextEditingController();
-  final confirmPasswordTextController=TextEditingController();
+  void signIn()async{
+    showDialog(
+      context: context,
+       builder: (context)=> const Center(
+        child:CircularProgressIndicator() ,
+        ),
+        );
+    try{
+ await FirebaseAuth.instance.signInWithEmailAndPassword(
+  email: emailTextController.text, 
+  password: passwordTextController.text,
+  );
+  if(context.mounted)Navigator.pop(context);
+  }on FirebaseAuthException catch(e){
+    Navigator.pop(context);
+    displayMessage(e.code);
+  }
+  }
+  void displayMessage(String message){
+    showDialog(
+      context: context, 
+      builder: (context)=> AlertDialog(
+        title: Text(message),
+      ),
+      );
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -33,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
              const SizedBox(height:50),
               Text(
-                "Let's create an account for you",
+                "Welcome Back,you have been missed!",
                 style: TextStyle(
                       color: Colors.grey[700],
                     ),
@@ -50,28 +76,22 @@ class _LoginPageState extends State<LoginPage> {
                  hintText: 'Password', 
                  obscureText:true,
                  ),
-                 const SizedBox(height: 10),
-                 MyTextField(
-                controller: confirmPasswordTextController,
-                 hintText: 'Confirm Password', 
-                 obscureText:true,
-                 ),
                  const SizedBox(height: 10), 
-                 MyButton(onTap: widget.onTap,
-                  text: 'Sign Up'
+                 MyButton(onTap: signIn,
+                  text: 'Sign In'
                   ),
                   const SizedBox(height: 25),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                    Text("Already have a account",
+                    Text("Not a member?",
                     style: TextStyle(
                       color: Colors.grey[700],
                     ),),
                     const SizedBox(width: 4),
                     GestureDetector(
                       onTap: widget.onTap,
-                      child:const Text("Login now",
+                      child:const Text("Register now",
                       style:TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.blue,
