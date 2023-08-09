db0624f3de695aa112850074e42cd0e108bc0af1
shrutisharma41 <140740010+shrutisharma41@users.noreply.github.com>
2023-08-09 23:04:54 +0530

    home_page.dart

--- pages/home_page.dart
+++ pages/home_page.dart
@@ -1,5 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:socialmedia_app/components/drawer.dart';
+import 'package:socialmedia_app/components/wall_post.dart';
+import 'package:socialmedia_app/helper/helper_methods.dart';
+import 'package:socialmedia_app/pages/profile_page.dart';
+
+import '../components/text_field.dart';
 class HomePage extends StatefulWidget {
+
   const HomePage ({super.key});
 
   @override
@@ -7,12 +16,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePagState extends State<HomePage> {
+  final currentUser=FirebaseAuth.instance.currentUser!;
+  final textController=TextEditingController();
+  void signOut(){
+    FirebaseAuth.instance.signOut();
+  }
+  void postMessage(){
+  if(textController.text.isNotEmpty){
+    FirebaseFirestore.instance.collection("User Posts").add({
+      'UserEmail':currentUser.email,
+      'Message':textController.text,
+      'TimeStamp':Timestamp.now(),
+      'Likes':[],
+    });
+  }
+  setState(() {
+    textController.clear();
+  });
+  }
+  void goToProfilePage(){
+    Navigator.pop(context);
+    Navigator.push(context,MaterialPageRoute(
+      builder: (context)=> const ProfilePage(),
+      ),
+      );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         title: const Text
-        ("The Wall"),),
+        ("EMPOWER CONNECT"),
+       
+       
+        ),
+        drawer: MyDrawer(
+          onProfileTap: goToProfilePage,
+          onSignOut: signOut,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                  .collection("User Posts")
+                  .orderBy(
+                    "TimeStamp",
+                    descending:false,
+                  )
+                  .snapshots(),
+                  builder: (context,snapshot){
+                    if(snapshot.hasData){
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder:(context,index){
+                     final post=snapshot.data!.docs[index];
+                        return WallPost(
+                          message: post['Message'],
+                           user: post['UserEmail'],
+                           postId: post.id,
+                           likes:List<String>.from(post['Likes']??[]),
+                           time: formatDate(post['TimeStamp']),
+                            );
+                      },
+                      );
+                    }else if(snapshot.hasError){
+                      return Center(
+                        child:Text('Error:${snapshot.error}'),
+                      );
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                      );
+                  },
+                  ),
+                ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: MyTextField(
+                        controller:textController,
+                        hintText:'Write something on the wall..',
+                        obscureText:false,
+                      ),
+                      ),
+                      IconButton(
+                        onPressed: postMessage,
+                        icon:const Icon(Icons.arrow_circle_up),
+                        ),
+                  ],
+                ),
+              ),
+           Text("Logged in as:" +currentUser.email!,
+           style: TextStyle(color:Colors.grey,
+           ),
+           ),
+           const SizedBox(width: 50,
+           ),
+            ],
+          ),
+        ),
     );
   }
 }
